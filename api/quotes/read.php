2295370bf960c87191a00779f56c69398ced03ae
adlund97 <112456558+adlund97@users.noreply.github.com>
2023-03-22 17:03:16 -0500

    Update read.php

--- api/quotes/read.php
+++ api/quotes/read.php
@@ -25,7 +25,6 @@ $num = $result->rowCount();
 // If there are quotes in database table
 if ($num > 0) {
     $quote_arr = array();
-    $quote_arr['data'] = array();
 
     while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
         extract($row);
@@ -37,7 +36,7 @@ if ($num > 0) {
             'category' => $category
         );
 
-        array_push($quote_arr['data'], $quote_item);
+        array_push($quote_arr, $quote_item);
     }
 
     echo json_encode($quote_arr);
